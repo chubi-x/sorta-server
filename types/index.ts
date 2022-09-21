129e22cd315b5b9ee90db74496f287f20e4d6923
chubi-x <chubixax@protonmail.com>
2022-09-21 12:31:50 +0100

    feat: declare types for query string

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,13 @@
 import { Session } from "express-session";
-import { IOAuth2RequestTokenResult } from "twitter-api-v2/dist/types";
+import { IOAuth2RequestTokenResult } from "twitter-api-v2/dist/types/auth.types";
+// import { IOAuth2RequestTokenResult } from "twitter-api-v2/dist/types";
 declare module "express-session" {
   interface Session {
     oAuth: IOAuth2RequestTokenResult;
+  }
+}
+declare module "qs" {
+  interface ParsedQs {
+    code: string;
   }
 }
