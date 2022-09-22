c3b92f9f7165e4b137cac5d82155a8eeb4097972
chubi-x <chubixax@protonmail.com>
2022-09-22 09:42:09 +0100

    feat: catch firebase errors

--- auth/firebase.ts
+++ auth/firebase.ts
@@ -2,10 +2,16 @@ import firebase from "firebase-admin";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const firebaseAdmin = firebase.initializeApp({
-  credential: firebase.credential.cert(
-    JSON.parse(process.env.GOOGLE_APPLICATIONS_CREDENTIALS!)
-  ),
-  databaseURL:
-    "https://sorta-twitter-app-default-rtdb.europe-west1.firebasedatabase.app",
-});
+try {
+  firebase.initializeApp({
+    credential: firebase.credential.cert(
+      JSON.parse(process.env.GOOGLE_APPLICATIONS_CREDENTIALS!)
+    ),
+    databaseURL:
+      "https://sorta-twitter-app-default-rtdb.europe-west1.firebasedatabase.app",
+  });
+} catch (err) {
+  //  TODO: return the error to a loggin service
+  console.log(err);
+}
+export const firebaseDb = firebase.database();
