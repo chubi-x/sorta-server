d12f9e2ce0cb02b43e4f70eef0b40761b3be1303
chubi-x <chubixax@protonmail.com>
2022-09-23 18:09:16 +0100

    feat: install and use body parser

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
 import { router } from "./routes/routes";
 // initialize express app
 const app: Express = express();
+// use body parser
+app.use(bodyParser.json());
 // use routes
 app.use(router);
 
