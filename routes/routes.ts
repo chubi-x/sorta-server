e2f0bf50a4137320278bae01656bd8e6731dd360
chubi-x <chubixax@protonmail.com>
2022-09-27 17:05:13 +0100

    fix: wrap entire bookmarks endpoint in try-catch

--- routes/routes.ts
+++ routes/routes.ts
@@ -180,17 +180,17 @@ router.get("/user", (req: Request, res: Response) => {
 });
 // get bookmarks
 router.get("/bookmarks", async (req: Request, res: Response) => {
-  // route only works if user has a session
-  // retrieve the user username from the session store
-  const userId = req.session.userId;
-  if (userId) {
-    console.log("a session was found!");
-    // get a db ref
-    const userIdRef = usersRef.child(userId);
-    userIdRef.once(
-      "value",
-      async (snapshot) => {
-        try {
+  try {
+    // route only works if user has a session
+    // retrieve the user username from the session store
+    const userId = req.session.userId;
+    if (userId) {
+      console.log("a session was found!");
+      // get a db ref
+      const userIdRef = usersRef.child(userId);
+      userIdRef.once(
+        "value",
+        async (snapshot) => {
           // get user access token
           const user = snapshot.val();
           const accessToken = user.accessToken;
@@ -198,23 +198,25 @@ router.get("/bookmarks", async (req: Request, res: Response) => {
           // get and return the users bookmarks
           const bookmarks = await newTwitterClient.v2.bookmarks();
           return res.status(200).json({ bookmarks });
-        } catch (err) {
-          // TODO: log error to logging service
-          console.error("the error: " + err);
-          // redirect to authorize
-          return res.redirect(303, "/authorize");
+        },
+        (errorObject) => {
+          // TODO: log error to logging serivce
+          console.log(
+            "couldn't retrieve the data \n" +
+              errorObject.name +
+              errorObject.message
+          );
+          return res.end(
+            "error could not retrieve your data. please try again."
+          );
         }
-      },
-      (errorObj) => {
-        // TODO: log error to logging serivce
-        console.log(
-          "couldn't retrieve the data \n" + errorObj.name + errorObj.message
-        );
-        return res.end("error could not retrieve your data. please try again.");
-      }
-    );
-  } else {
-    return res.redirect(303, "/authorize");
+      );
+    } else {
+      return res.redirect(303, "/authorize");
+    }
+  } catch (err) {
+    // TODO: log to logging service
+    console.log(`Error accessing bookmarks endpoint. see below \n ${err}`);
   }
 });
 // route to remove a bookmark
