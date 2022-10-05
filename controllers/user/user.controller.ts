0ae8db696c80e2a8d019109c7eb9470d25f1de40
chubi-x <chubixax@protonmail.com>
2022-10-05 13:09:22 +0100

    chore: improve server error messages

--- controllers/user/user.controller.ts
+++ controllers/user/user.controller.ts
@@ -10,22 +10,45 @@ userRouter.get("/", hasSession, async (req: Request, res: Response) => {
     const userId = req.session.userId;
     // retrieve user from db
     const userRef = usersRef.child(userId);
-    await userRef.once("value", (snapshot) => {
-      const userData = snapshot.val();
-      ResponseHandler.requestSuccessful({
-        res,
-        payload: {
-          user: { id: userId, username: userData.username, pfp: userData.pfp },
-        },
-        message: "User data retrieved successfully",
-      });
-    });
+    await userRef.once(
+      "value",
+      (snapshot) => {
+        const userData = snapshot.val();
+        ResponseHandler.requestSuccessful({
+          res,
+          payload: {
+            user: {
+              id: userId,
+              username: userData.username,
+              pfp: userData.pfp,
+            },
+          },
+          message: "User data retrieved successfully",
+        });
+      },
+      (err) => {
+        if (err) {
+          console.log(
+            `There was an error retrieving user data. See below\n ${err}`
+          );
+          ResponseHandler.serverError(
+            res,
+            "There was a problem retrieving your data. Please try again."
+          );
+        }
+      }
+    );
   } catch (err) {
+    // TODO: log to logging service
     console.log(
       `There was an error accessing GET user endpoint. see below\n ${err}`
     );
-    ResponseHandler.serverError(res);
+    ResponseHandler.serverError(
+      res,
+      "There was a problem retrieving your data. Please try again."
+    );
   }
 });
 
 export { userRouter };
+
