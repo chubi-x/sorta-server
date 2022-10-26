295aa766ff679bc0d2dfe1c2eb3c5b8ce3706c10
chubi-x <chubixax@protonmail.com>
2022-10-26 14:34:04 +0100

    chore: return response if user has no session

--- middleware/hasSession.ts
+++ middleware/hasSession.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from "express";
-
+import { ResponseHandler } from "../services";
 //  session checker middleware
 export function hasSession(req: Request, res: Response, next: NextFunction) {
   if (!req.session.userId) {
-    return res.redirect("/authorize");
+    return ResponseHandler.clientError(res, "User not logged in.");
   } else {
     return next();
   }
