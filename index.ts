b8eab535d16029f547b18dbe24dc536c97081919
chubi-x <chubixax@protonmail.com>
2022-10-26 14:31:36 +0100

    setup https

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import express, { Express, NextFunction, Request, Response } from "express";
 import bodyParser from "body-parser";
 import session from "express-session";
 import cookieParser from "cookie-parser";
+import https from "https";
+import fs from "fs";
 import {
   bookmarkRouter,
   categoryRouter,
@@ -12,24 +14,11 @@ import {
 const app: Express = express(),
   // cookie age
   threeDays = 1000 * 60 * 60 * 72;
-// use body parser
-app.use(bodyParser.json());
-
-// session middleware
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET!,
-    saveUninitialized: true,
-    cookie: {
-      maxAge: threeDays,
-      httpOnly: false,
-    },
-    resave: false,
-  })
-);
-// use cookie parser
-app.use(cookieParser());
-
+// setup local https
+const options = {
+  key: fs.readFileSync("./config/localhost+2-key.pem"),
+  cert: fs.readFileSync("./config/localhost+2.pem"),
+};
 // set CORS header
 app.use((req: Request, res: Response, next: NextFunction) => {
   // for vite dev environment
@@ -37,11 +26,33 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With,Content-Type,Accept"
+    "Origin,X-Requested-With,Content-Type,Accept"
   );
-  res.header("Access-Control-Allow-Credentials");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
   next();
 });
+
+// use body parser
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+// use cookie parser
+app.use(cookieParser());
+
+// session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET!,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: threeDays,
+      httpOnly: false,
+      sameSite: "none",
+      secure: true,
+    },
+    resave: false,
+  })
+);
+
 // use routes
 app.use("/", authRouter);
 app.use("/user", userRouter);
@@ -50,9 +61,10 @@ app.use("/categories", categoryRouter);
 
 // PORT
 const PORT = process.env.PORT;
-const server = app.listen(PORT, () => {
+
+https.createServer(options, app).listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
-
-export { server, app };
+// const server = app.listen(PORT, () => {
+// });
 
