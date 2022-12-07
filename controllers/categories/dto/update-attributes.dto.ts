ea1af0b4d166cc0f278c0061e61a398a50b94009
Chubiyojo Adejoh <chubixax@protonmail.com>
2022-12-07 14:48:26 +0100

    fix: remove contradictory class validator

--- controllers/categories/dto/update-attributes.dto.ts
+++ controllers/categories/dto/update-attributes.dto.ts
@@ -2,17 +2,15 @@ import { IsString, IsOptional, IsNotEmpty, IsUrl } from "class-validator";
 export class UpdateCategoryAttributesDto {
   @IsString()
   @IsOptional()
-  @IsNotEmpty()
   name?: string;
 
   @IsString()
   @IsOptional()
-  @IsNotEmpty()
   description?: string;
 
   @IsString()
   @IsUrl()
   @IsOptional()
-  @IsNotEmpty()
   image?: string;
 }
+
