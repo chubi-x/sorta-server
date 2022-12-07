1169c879cea8cf83e7da0b23c12eddf8b8f5975c
Chubiyojo Adejoh <chubixax@protonmail.com>
2022-12-07 15:35:43 +0100

    feat; provide type declarations

--- declaration.d.ts
+++ declaration.d.ts
@@ -7,3 +7,29 @@ interface User {
   tokenExpiresIn: number;
   verified: boolean;
 }
+
+interface Bookmarks {
+  data: Bookmark[];
+  meta: object;
+}
+
+interface Bookmark {
+  text: string;
+  id: string;
+  author_id: string;
+  author_name: string;
+  author_pfp: string;
+  author_username: string;
+  author_verified: string;
+  created_at: string;
+  entities: {
+    urls: TweetEntityUrl[];
+  };
+}
+interface Category {
+  id: string;
+  name: string;
+  description: string;
+  image?: string;
+  bookmarks: Bookmark[];
+}
