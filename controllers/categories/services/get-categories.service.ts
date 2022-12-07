acd026c996d17c7e961be4b3acb91b20f1beb926
Chubiyojo Adejoh <chubixax@protonmail.com>
2022-12-07 14:53:24 +0100

    refactor: change location of read bookmarks function

--- controllers/categories/services/get-categories.service.ts
+++ controllers/categories/services/get-categories.service.ts
@@ -2,6 +2,33 @@ import { Request, Response } from "express";
 import { Reference, DataSnapshot } from "@firebase/database-types";
 import { ResponseHandler } from "../../../services";
 
+async function readBookmarks(
+  categorySnapshot: DataSnapshot,
+  bookmarksRef: Reference,
+  categoriesArray: any[]
+) {
+  const bookmarksArray: string[] = [];
+  const categoryId = categorySnapshot.ref.key;
+  await bookmarksRef.once(
+    "value",
+    (bookmarksSnapshot) => {
+      bookmarksSnapshot.forEach((bookmarkSnapshot) => {
+        if (bookmarkSnapshot.child("categoryId").val() === categoryId)
+          bookmarksArray.push(bookmarkSnapshot.child("tweetId").val());
+      });
+    },
+    (errorObject) => {
+      // TODO: log to logging service
+      console.log(
+        `error accessing bookmarks \n ${errorObject.name} : ${errorObject.message}`
+      );
+    }
+  );
+  categoriesArray.push({
+    ...{ id: categoryId, ...categorySnapshot.val() },
+    bookmarks: bookmarksArray,
+  });
+}
 export async function getCategories(
   req: Request,
   res: Response,
@@ -19,42 +46,19 @@ export async function getCategories(
       "value",
       async (categoriesSnapshot) => {
         // list to hold bookmarks ref read promises
-        const promiseList: Promise<any>[] = [],
-          // async function to read bookmarks ref
-          readBookmarks = async (categorySnapshot: DataSnapshot) => {
-            const bookmarksArray: string[] = [],
-              categoryId = categorySnapshot.ref.key;
-            await bookmarksRef.once(
-              "value",
-              (bookmarksSnapshot) => {
-                bookmarksSnapshot.forEach((bookmarkSnapshot) => {
-                  if (bookmarkSnapshot.child("categoryId").val() === categoryId)
-                    bookmarksArray.push(
-                      bookmarkSnapshot.child("tweetId").val()
-                    );
-                });
-              },
-              (errorObject) => {
-                // TODO: log to logging service
-                console.log(
-                  `error accessing bookmarks \n ${errorObject.name} : ${errorObject.message}`
-                );
-              }
-            );
-            categoriesArray.push({
-              category: { id: categoryId, data: categorySnapshot.val() },
-              bookmarks: bookmarksArray,
-            });
-          };
+        const promiseList: Promise<any>[] = [];
+        // async function to read bookmarks ref
         categoriesSnapshot.forEach((categorySnapshot) => {
-          promiseList.push(readBookmarks(categorySnapshot));
+          promiseList.push(
+            readBookmarks(categorySnapshot, bookmarksRef, categoriesArray)
+          );
         });
         // fulfill promises
         await Promise.all(promiseList);
         //TODO: return an array of the categories and their bookmarks
         return ResponseHandler.requestSuccessful({
           res,
-          payload: { categories: categoriesArray },
+          payload: [...categoriesArray],
         });
       },
       (errorObject) => {
